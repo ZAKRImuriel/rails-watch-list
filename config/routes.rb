5b8975065fc3405a8a1a7b519b7d8a36561dad0f
Muriel Zakri <murielzakri@gmail.com>
2023-02-24 15:41:28 +0100

    fix deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  resources :lists, only: [:new, :create , :index, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
+end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
